f6d774b27d8b246568670a4666453a108d830e14
thisisemre <emreyildiz183@gmail.com>
2024-11-07 19:19:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const app = express();
+
+require('dotenv').config(); 
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
